25373ecef4e83eb0b3894fce2d35bc3356ad4807
Casey Foster <c@sey.me>
2021-05-06 14:16:40 -0500

    Use sass over node-sass, 4.1.0

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import npath from 'path';
 import { promisify } from 'util';
 
-import sass from 'node-sass';
-import _ from 'underscore';
+import sass from 'sass';
 
 const render = promisify(sass.render);
 
@@ -15,19 +14,13 @@ const getRelative = path => npath.relative('.', path);
 
 export default async ({ file: { buffer, links, path }, options }) => {
   options = { ...DEFAULTS, ...options };
-  try {
-    const { css, stats } = await render({
-      ...options,
-      data: buffer.toString() || '\n',
-      includePaths: options.includePaths.concat(npath.dirname(path))
-    });
-    return {
-      buffer: Buffer.from(css),
-      links: links.concat(_.map(stats.includedFiles, getRelative))
-    };
-  } catch (er) {
-    throw _.extend(new Error(), er, {
-      message: `${path}: line ${er.line}, column ${er.column}, ${er.message}`
-    });
-  }
+  const { css, stats } = await render({
+    ...options,
+    data: buffer.toString() || '\n',
+    includePaths: options.includePaths.concat(npath.dirname(path))
+  });
+  return {
+    buffer: Buffer.from(css),
+    links: links.concat(stats.includedFiles.map(getRelative))
+  };
 };
